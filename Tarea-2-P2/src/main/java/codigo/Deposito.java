eb2c4e357fe4614d24bb04c9aaf12dab9ae5b252
Juan Agustin Umaña Silva <juan.agustinsilva1@gmail.com>
2023-10-16 21:40:45 -0300

    Merge entre ramas

--- Tarea-2-P2/src/main/java/codigo/Deposito.java
+++ Tarea-2-P2/src/main/java/codigo/Deposito.java
@@ -2,17 +2,18 @@
 package codigo;
 
 import java.util.ArrayList;
-
-public class Deposito<T>{
+public class Deposito<T> {
     private ArrayList<T> array;
-    public Deposito(){
+
+    public Deposito() {
         array = new ArrayList();
     }
 
     public void addProducto(T nueva) {
         array.add(nueva);
     }
-    public T getProducto(){ //pese a que es getproducto tmbn puede servir como get moneda
+
+    public T getProducto() { //pese a que es getproducto tmbn puede servir como get moneda
         if (array.size() != 0) {
             return array.remove(0);
         }
