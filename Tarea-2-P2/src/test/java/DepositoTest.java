ba8fba0654bfd0384b93d903232ae2ff8117eb31
alanyur <alanibagon@gmail.com>
2023-10-18 14:48:52 -0300

    actualización de rama main a partir de repositorio online

--- Tarea-2-P2/src/test/java/DepositoTest.java
+++ Tarea-2-P2/src/test/java/DepositoTest.java
@@ -18,5 +18,20 @@ public class DepositoTest {
         int cantidad = bebidaDeposito.size();
         Assertions.assertNotEquals(0,cantidad);
     }
-
+    @Test
+    public void testGetProductoBebida() {
+        //para hacer esta prueba usamos una bebida generica
+        Bebida generica = new Bebida(1234) {
+            @Override
+            public String getSabor() {
+                return "generica";
+            }
+        };
+        Deposito<Bebida> bebidaDeposito = new Deposito<>();
+        bebidaDeposito.addProducto(generica);
+        int cantidad = bebidaDeposito.size();
+        bebidaDeposito.getProducto();
+        cantidad = bebidaDeposito.size();
+        Assertions.assertEquals(0, cantidad);
+    }
 }
